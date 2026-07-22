c56e50977b25136c3ee9353054f9ea379848c6b1
Muhammed Cemil CAKA <152091278+muhammed-cemil-caka@users.noreply.github.com>
2026-07-22 10:43:51 +0300

    tasarım değişikliği sorunu çözüldü

---
+++ content.js
@@ -0,0 +1,4 @@
+// İZBAN Admin Panel Modernizer extension content script
+// Gelişmiş tasarım tamamen CSS ile yönetilmektedir. 
+// AJAX ve dinamik menü geçişlerinde çakışmaları tamamen önlemek için DOM müdahalesi yapılmamaktadır.
+console.log("İZBAN Admin Modernizer Aktif.");
